b9a44ffae5938c4eafe1772119f81e83138236b2
Sachin <145639140+Derikklok@users.noreply.github.com>
2025-08-22 05:46:55 +0530

    Add Bootstrap integration and new components for roles and designation

---
+++ src/app/components/roles/roles.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-roles',
+  imports: [FormsModule],
+  templateUrl: './roles.component.html',
+  styleUrl: './roles.component.css',
+})
+export class RolesComponent {
+  firstName: string = 'Sachin Pasindu';
+  lastName = 'Pitigala';
+  age: number = 18;
+  isActive:boolean = false;
+  currentDate : Date = new Date();
+  inputType:string = "checkbox";
+}
